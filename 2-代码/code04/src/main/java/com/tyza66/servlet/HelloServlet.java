969aded6d7fd5085a297610ef743032be71d2c97
洮羱芝闇 <shun_@outlook.com>
2022-06-17 20:25:05 +0800

    update

---
+++ 2-代码/code04/src/main/java/com/tyza66/servlet/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.tyza66.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.print("Hello,World!123");
+    }
+}
