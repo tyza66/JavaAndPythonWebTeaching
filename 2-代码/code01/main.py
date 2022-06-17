ac17a8558420558a90a6ccf673e6f4a04e10f011
洮羱芝闇 <shun_@outlook.com>
2022-06-17 18:22:31 +0800

    update

--- 2-代码/code01/main.py
+++ 2-代码/code01/main.py
@@ -1,1 +1,36 @@
+import flask
 print('Hello,World!')
+a = "你好"
+print(a)
+a = 2
+print(a)
+if 1 != 1:
+    print(2)
+    print(3)
+print(4)
+
+print('---------------')
+
+# print(21)
+'''
+你好
+'''
+
+b = (1, 2, 3)
+c = [1, 2, 3]
+d = [[1], [1, 2], [1, 2, .3]]
+print(d[2][2])
+c[0] = 100
+print(c[0])
+
+print('---------------')
+
+e = {
+    "a": b,
+    "bcc": c,
+    "c": "233"
+}
+
+print(e['a'])
+
+
