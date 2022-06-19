6f1268f1490d4aad8da9bc52b727096021e46a98
洮羱芝闇 <shun_@outlook.com>
2022-06-19 19:13:00 +0800

    update

---
+++ 2-代码/code04/src/main/java/com/tyza66/test2/TextDemo.java
@@ -0,0 +1,10 @@
+package com.tyza66.test2;
+
+import org.junit.jupiter.api.Test;
+
+public class TextDemo {
+    @Test
+    public void textMySql(){
+        //jdbc连接MySQl数据库
+    }
+}
--- README.md
+++ README.md
@@ -9,5 +9,8 @@
 4. 第一个pyweb应用上
 5. 第一个pyweb应用下
 6. 使用maven构建一个Javaweb应用
+7. 单元测试和mysql安装上
+8. 单元测试和mysql安装中
+9. 单元测试和mysql安装下
 
 By:tyza66(洮羱芝闇)
