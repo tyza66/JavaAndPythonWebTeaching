26fb856cbf65a6db5f91eb9670a391f8ca68feec
洮羱芝闇 <shun_@outlook.com>
2022-07-22 12:35:10 +0800

    update

--- 2-代码/code04/src/main/java/com/tyza66/test2/TextDemo.java
+++ 2-代码/code04/src/main/java/com/tyza66/test2/TextDemo.java
@@ -3,14 +3,23 @@ package com.tyza66.test2;
 import jdk.nashorn.internal.runtime.ECMAException;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class TextDemo {
     @Test
     public void textMySql(){
         //jdbc连接MySQl数据库
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "";
+            String user = "";
+            String password = "";
+            Connection conn = DriverManager.getConnection(url,user,password);
         }
-        catch (ECMAException | ClassNotFoundException e){
+        catch (ClassNotFoundException | SQLException e){
             System.out.println(e);
         }
     }
